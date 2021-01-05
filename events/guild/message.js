2aadbbb18d14877609a061c246b82ed158eb73da
[Darbuzio12312 <arbuzio11@o2.pl>
2021-01-06 00:25:45 +0100

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const chalk = require('chalk');
-const config = require('.../.../config.json');
+const config = require('../../config.json');
 const prefix = config.prefix;
 
 module.exports = async (client, message) => {
