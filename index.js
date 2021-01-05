9dc9b2bde4568785128f34dc5985cd86354b26cb
[Darbuzio12312 <arbuzio11@o2.pl>
2021-01-05 21:51:45 +0100

    nowy command handler

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix } = require('./config.json');
-const fs = require('fs');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const tak = '<a:ta:782965898885333032>';
@@ -12,27 +11,14 @@ const dino = '<:DinoWithHeart:792473366447521833>';
 
 client.login(process.env.TOKEN)
 
-for(const file of commandFiles){
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
-
-client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if(!client.commands.has(command)) return;
-  try {
-      client.commands.get(command).execute(msg, args);
-  }catch(error){
-      console.error(error);
-      message.reply('Wystąpił błąd'+`${SadDino}`)
-  }
-})
-
+client.on('message', msg => {
+  client.commands = new Discord.Collection();
+  client.aliases = new Discord.Collection();
+  client.queue = new Map();
+  ['command', 'event'].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+  });
+});
 
 client.on('ready', () => {
   console.log(`Zalogowano jako ${client.user.tag}!!`);
