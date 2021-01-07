3249fdae733b568393ed60ffc99f077ca5f36cad
[Darbuzio12312 <arbuzio11@o2.pl>
2021-01-07 16:31:08 +0100

    naprawa erroru

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,44 +1,20 @@
 const Discord = require('discord.js');
-const chalk = require('chalk');
-const prefix = '$';
-<<<<<<< HEAD
-=======
+const prefix = process.env.PREFIX;
 
->>>>>>> parent of d70d5f5... oryginalność
 module.exports = async (client, message) => {
-    try {
-        const queue = new Map();
-        if (message.author.bot) return;
-        if (!message.guild) return;
-        if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
-            const embed = new Discord.MessageEmbed()
-            embed.setTitle(`Witaj!`, message.guild.iconURL())
-            embed.setColor('RANDOM')
-            embed.setDescription("Jestem " + client.user.username + "! Mój prefix to `$` wpisz `" + prefix + 'Help` żeby zobaczyć wszystkie komendy')
-            embed.setTimestamp()
-            return message.channel.send(embed);
-        }
+ try {
+  const queue = new Map();
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
+   const embed = new Discord.MessageEmbed()
+    .setTitle(`Hi!`, message.guild.iconURL())
+    .setColor('RANDOM')
+    .setDescription("I'm a " + client.user.username + "! My prefix is \`" + prefix + "\` To see all commands please type \`" + prefix + " help\`")
+    .setTimestamp()
+   message.channel.send(embed);
+  }
 
-<<<<<<< HEAD
-        if (!message.content.startsWith(prefix)) return;
-        if (!message.member) message.member = await message.guild.fetchMember(message);
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const cmd = args.shift().toLowerCase();
-        if (cmd.length === 0) return;
-        let command = client.commands.get(cmd);
-        if (!command) command = client.commands.get(client.aliases.get(cmd));
-        if (!command) {
-            return message.channel.send({ embed: { color: 16734039, description: "Ta komenda nie istnieje, wpisz `" + `${prefix}` + "help` żeby poznać komendy" } });
-        }
-
-        if (command) {
-            return command.run(client, message, args);
-        }
-    } catch (err) {
-        console.log(err);
-        return message.channel.send({ embed: { color: 16734039, description: "That command does not exist, Take a look at " + `${prefix}` + " help!" } });
-    }
-=======
   if (!message.content.startsWith(prefix)) return;
   if (!message.member) message.member = await message.guild.fetchMember(message);
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -47,15 +23,21 @@ module.exports = async (client, message) => {
   let command = client.commands.get(cmd);
   if (!command) command = client.commands.get(client.aliases.get(cmd));
   if (!command) {
-   return message.channel.send({embed: {color: 16734039,description:"Ta komenda nie istnieje, wpisz `" + `${prefix}` + "help` żeby poznać komendy"}});
+   message.channel.send({embed: {
+    color: 16734039,
+    description:
+    "That command does not exist, Take a look at " + `${prefix}` + " help!"
+   }});
   }
 
   if (command) {
-   return command.run(client, message, args);
+   command.run(client, message, args);
   }
  } catch (err) {
   console.log(err);
-  return message.channel.send({embed: {color: 16734039,description: "That command does not exist, Take a look at " + `${prefix}` + " help!"}});
+  message.channel.send({embed: {
+   color: 16734039,
+   description: "That command does not exist, Take a look at " + `${prefix}` + " help!"
+  }});
  }
->>>>>>> parent of d70d5f5... oryginalność
 }
