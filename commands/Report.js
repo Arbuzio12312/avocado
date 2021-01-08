9e3424b284a159556014bac8dda05f4f14dc865c
[Darbuzio12312 <arbuzio11@o2.pl>
2021-01-08 22:56:25 +0100

    Utworzenie komendy $report

---
+++ commands/Report.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js');
+const tak = '<a:ta:782965898885333032>';
+const nie = '<a:ne:792020013023232021>';
+const Rola1 = '782728348718596096';
+const SadDino = '<:dinosad:792473409715961857>';
+const dino = '<:DinoWithHeart:792473366447521833>';
+
+module.exports = {
+    name: "Report",
+    aliases: ["report", "Report"],
+    category: "pomoc",
+    description: "Zglasza uzytkownika",
+    cooldown: 0,
+    usage: "$Report @user [powod]",
+    run: async (client, msg, args) => {
+        
+        const zgloszony = msg.mentions.members.first();
+        const powod = args.slice(1).join(" ");
+
+        if (msg.member.roles.cache.some((role) => role.id === `${rola1}`)) {
+            msg.react(`${nie}`);
+            return msg.author.send('Najpierw zarejestruj się'+`${nie}`);
+        }
+
+        if(!zgloszony) {
+            msg.react(`${nie}`);
+            return msg.reply('Musisz oznaczyć użytkownika'+`${nie}`);
+        }
+
+        if (zgloszony.roles.cache.some((role) => role.id === '796418823657750590')) {
+            msg.react(`${SadDino}`);
+            return msg.reply('Chciałeś mnie zgłosić?...'+`${SadDino}`);
+        }
+
+        if (zgloszony.roles.cache.some((role) => role.id === '775090536432402432')) {
+            msg.react(`${nie}`);
+            return msg.reply('Nie możesz zgłosić bota'+`${nie}`);
+        }
+
+        if (!powod) {
+            msg.react(`${nie}`);
+            msg.reply('Musisz podać powód'+`${nie}`);
+        }
+
+        msg.react(`${tak}`);
+        msg.reply('Zgłoszenie zostało wysłane do administracji!'+`${tak}`);
+        msg.guild.channels.cache.get('797177518691844097').send('Nowe zgłoszenie! <@&789561458521800764>');
+
+        const zgloszenie = new Discord.MessageEmbed;
+
+        zgloszenie.setTitle('**ZGŁOSZENIE**');
+        zgloszenie.setDescription(`${msg.author.tag} `+'zgłasza '+`${zgloszony} `+'\n POWÓD: \n `'+`${powod}`+'`');
+        zgloszenie.setColor('#a900f7');
+        zgloszenie.setFooter(msg.member.user.username, msg.author.displayAvatarURL({ dynamic : true }));
+
+        msg.guild.cache.ger('797177518691844097').send(zgloszenie);
+        
+    }
+}
---
+++ commands/unTester.js
@@ -0,0 +1,28 @@
+const tak = '<a:ta:782965898885333032>';
+const nie = '<a:ne:792020013023232021>';
+const Rola1 = '782728348718596096';
+const SadDino = '<:dinosad:792473409715961857>';
+const dino = '<:DinoWithHeart:792473366447521833>';
+
+module.exports = {
+    name: "unTester",
+    aliases: ["unTester", "untester"],
+    description: "przestajesz byc testerem",
+    cooldown: 0,
+    category: "pomoc",
+    usage: "$unTester",
+    run: async (client, msg, args) => {
+        if (msg.member.roles.cache.some((role) => role.id == `${Rola1}`)) {
+            msg.react(`${nie}`);
+            return msg.author.send('Najpierw zarejestruj się!' + `${nie}`);
+        }
+        if (!msg.member.roles.cache.some((role) => role.id === '782961436384428033')) {
+            msg.react(`${nie}`);
+            return msg.author.send('Nie jesteś testerem!' + `${nie}`);
+        }
+        msg.react(`${tak}`);
+        msg.member.roles.remove('782961436384428033');
+        msg.author.send('Przestałeś być testerem!' + `${SadDino}`);
+
+    }
+}
