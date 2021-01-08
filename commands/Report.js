fe81a8cc39d298844686984555d171fd13b8c8a1
[Darbuzio12312 <arbuzio11@o2.pl>
2021-01-08 23:09:38 +0100

    Update Report.js

--- commands/Report.js
+++ commands/Report.js
@@ -53,7 +53,7 @@ module.exports = {
         zgloszenie.setColor('#a900f7');
         zgloszenie.setFooter(msg.member.user.username, msg.author.displayAvatarURL({ dynamic : true }));
 
-        msg.guild.cache.get('797177518691844097').send(zgloszenie);
+        msg.guild.channels.cache.get('797177518691844097').send(zgloszenie);
         
     }
 }
