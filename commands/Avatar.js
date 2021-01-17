f95267ac082e152e70fbaa44066066b88cda661f
[Darbuzio12312 <arbuzio11@o2.pl>
2021-01-17 11:50:24 +0100

    Update Avatar.js

--- commands/Avatar.js
+++ commands/Avatar.js
@@ -33,7 +33,7 @@ module.exports = {
 
         info.setTitle(`${user.username}`+' avatar');
         info.setColor('RANDOM');
-        info.setImage(member.user.displayAvatarURL({dynamic: true}));
+        info.setImage(user.displayAvatarURL({dynamic: true}));
 
         msg.channel.send(info);
     }
