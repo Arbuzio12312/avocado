592384b5c309aff4a7e3818bf94205cc18d92218
[Darbuzio12312 <arbuzio11@o2.pl>
2021-03-02 17:28:52 +0100

    Update Help.js

--- commands/Help.js
+++ commands/Help.js
@@ -13,6 +13,17 @@ module.exports = {
     description: "komenda od pomocy",
     usage: "$Help",
     run: async (client, msg, args) => {
+        if (msg.member.roles.cache.some((role) => role.id === '816327936629669971')) {
+            msg.react(`${nie}`);
+            const akysz = new Discord.MessageEmbed;
+            akysz.setColor('#000000');
+            akysz.setTitle('HELP OF BLACK HOLE');
+            akysz.setDescription('$unlock <code> - wracasz do klatki')
+            akysz.setImage('https://cdn.discordapp.com/attachments/679843963762769975/795746859015733288/unknown.png');
+            akysz.setFooter(msg.member.user.username, msg.author.displayAvatarURL({ dynamic: true }));
+            msg.channel.send(embed);
+            return msg.channel.send(akysz);
+        }
         if (msg.member.roles.cache.some((role) => role.id == `${Rola1}`)) {
             msg.react(`${nie}`);
             return msg.author.send('Najpierw zarejestruj się! ' + `${nie}`);
@@ -24,7 +35,7 @@ module.exports = {
         embed.setDescription('**KOMENDY PODSTAWOWE** \n `$Help` - pokazuje wszystkie komendy \n `$Porn` - umożliwia oglądanie porna \n `$unPorn` - usuwa możliwość oglądania porna \n `$Tester` - zostajesz testerem pewnych rzeczy \n `$unTester` - przestajesz być testerem \n `$Report` <użytkownik> <powód> - zgłaszasz innego członka serwera \n `$Avatar` <użytkownik> - pokazuje czyjś avatar \n \n **KOMENDY ADMINISTRATORA** \n `$Kwarantanna` <użytkownik> <powód> - wysyła użytkownika do klatki \n `$unKwarantanna` <użytkownik> - wypuszcza użytkownika z klatki');
         embed.setFooter(msg.member.user.username, msg.author.displayAvatarURL({ dynamic: true }));
         msg.channel.send(embed);
-        const informacja = new Discord.MessageEmbed();
+        const informacja = new Discord.MessageEmbed;
         informacja.setColor('RANDOM');
         informacja.setTitle('**INFORMACJA**');
         informacja.setDescription('Bot jest wciąż podczas robienia'+`${dino}`+'\n Więc proszę o małe oczekiwania co do bota');
