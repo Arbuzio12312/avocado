0e57a7a3b3cac4175273cd63569b02f67c4140b3
[Darbuzio12312 <arbuzio11@o2.pl>
2021-03-02 18:28:30 +0100

    komenda unlock

---
+++ commands/Unlock.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const tak = '<a:ta:782965898885333032>';
+const nie = '<a:ne:792020013023232021>';
+const Rola1 = '782728348718596096';
+const SadDino = '<:dinosad:792473409715961857>';
+const dino = '<:DinoWithHeart:792473366447521833>';
+
+module.exports = {
+    name: "Unlock",
+    category: "proznia",
+    aliases: ["Unlock", "unlock"],
+    description: "wychodzisz z próżni",
+    cooldown: 0,
+    usage: "$Unlock <code>",
+    run: async (client, msg, args) => {
+        if (!msg.member.roles.cache.some((role) => role.id === '816327936629669971')) {
+            return msg.reply(':)');
+        }
+        msg.reply(">:3");
+    }
+}
