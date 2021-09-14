cbb80f9a253bbcb1b872d484225c3ba92353549a
[Darbuzio12312 <arbuzio11@o2.pl>
2021-09-14 19:11:54 +0200

    Dokończenie komendy
    dokończenie komendy "mute"

--- commands/Mute.js
+++ commands/Mute.js
@@ -7,7 +7,7 @@ const dino = '<:DinoWithHeart:792473366447521833>';
 
 module.exports = {
     name: "Mute",
-    aliases: ["mute", "Mute"],
+    aliases: ["mute", "Mute", "m", "M"],
     description: "Wycisza jakiegos kretyna",
     category: "admin",
     cooldown: 0,
@@ -48,7 +48,38 @@ module.exports = {
 
         const powod = args.slice(1).join(" ");
         
-        msg.reply(`${powod}`);
+        if (powod === '') {
 
+            const ogloszenie = new Discord.MessageEmbed;
+
+            ogloszenie.setTitle('**UŻYTKOWNIK ZOSTAŁ WYCISZONY**');
+            ogloszenie.setDescription(`${kretyn}` + ' został/a wyciszony/a przez' + ` <@${msg.author.id}>` + '\n Powód: ' + `${powod}`);
+            ogloszenie.setColor('#ff1500');
+            ogloszenie.setFooter(msg.member.user.username, msg.author.displayAvatarURL({ dynamic : true }));
+
+            info.setTitle('**MUTE**');
+            info.setDescription(`<@${msg.author.id}>` + 'cie wyciszył/a \n Powód: ' + `${powod}`);
+            info.setColor('#ff1500');
+            info.setFooter('Administrator' + msg.member.user.username, msg.author.displayAvatarURL({ dynamic : true }))
+            
+            kretyn.send(info);
+            msg.guild.channels.cache.get('789564676006477854').send(ogloszenie);
+        }
+        
+        const ogloszenie = new Discord.MessageEmbed;
+        const info = new Discord.MessageEmbed;
+
+        info.setTitle('**MUTE**');
+        info.setDescription(`<@${msg.author.id}>` + 'cie wyciszył/a \n Powód: ' + `${powod}`);
+        info.setColor('#ff1500');
+        info.setFooter('Administrator' + msg.member.user.username, msg.author.displayAvatarURL({ dynamic : true }))
+        
+        ogloszenie.setTitle('**UŻYTKOWNIK ZOSTAŁ WYCISZONY**');
+        ogloszenie.setDescription(`${kretyn} ` + 'został/a wyciszony/a przez ' + `<@${msg.author.id}>` + '\n Powód: ' + `${powod}`);
+        ogloszenie.setColor('#ff1500');
+        ogloszenie.setFooter(msg.member.user.username, msg.author.displayAvatarURL({ dynamic : true }));
+
+        msg.guild.channels.cache.get('789564676006477854').send(ogloszenie);
+        kretyn.send(info);
     }
 }
